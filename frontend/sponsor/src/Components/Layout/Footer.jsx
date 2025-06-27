74e31622cb5402065ce9d9c678bb6544d9b79e9e
Dharmesh Yadav <166201809+Dharmesh-yadavo@users.noreply.github.com>
2025-06-27 17:19:51 +0530

    footer section done

--- frontend/sponsor/src/Components/Layout/Footer.jsx
+++ frontend/sponsor/src/Components/Layout/Footer.jsx
@@ -1,3 +1,11 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTwitter,
+  faInstagram,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
 export const Footer = () => {
   return (
     <>
@@ -5,25 +13,58 @@ export const Footer = () => {
         <div className="footer-container container">
           <div className="content_1">
             <img src="./images/logo.png" alt="logo" />
-            <p>text</p>
+            <p>site description / moto</p>
+          </div>
+          <div className="content_2">
+            <h4>NAVIGATION</h4>
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Explore</a>
+            <a href="#">Sales & Discount</a>
           </div>
           <div className="content_3">
-            <h4>Experience</h4>
-            <a href="./contact.html">Contact Us</a>
-            <a href="" target="_blank">
-              Payment Method
-            </a>
-            <a href="" target="_blank">
-              Delivery
-            </a>
-            <a href="" target="_blank">
-              Return and Exchange
-            </a>
+            <h4>Resource</h4>
+            <a href="./contact.html">FAQ</a>
+          </div>
+          <div className="content_4">
+            <h4>Contact Us</h4>
+            <p>datasmithss@gmail.com</p>
           </div>
         </div>
         <div className="f-design">
+          <div className="social-container">
+            <a
+              href="https://twitter.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faTwitter} className="icon" />
+            </a>
+            <a
+              href="https://twitter.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faInstagram} className="icon" />
+            </a>
+            <a
+              href="https://twitter.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faEnvelope} className="icon" />
+            </a>
+            <a
+              href="https://twitter.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faLinkedin} className="icon" />
+            </a>
+          </div>
           <div className="f-design-txt">
-            <p>Design and Code by Data Smiths </p>
+            <p>&copy; 2025 Site Name. All rights reserved.</p>
+            <p>Made with ❤️ by the Data Smiths</p>
           </div>
         </div>
       </footer>
